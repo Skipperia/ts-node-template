0e9beee13d3915858e9a83c77e1ada5be246300d
Alex Kipper <kippera27@gmail.com>
2024-04-07 15:18:42 +0300

    minor changes

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import PageContent from '../components/PageContent/PageContent';
 
 const HomePage: React.FC = () => {
-  const pageContent = "testing2";
+  const pageContent = "testing3";
 
   return (<div>
     <PageContent pageTitle={"HomePage"} pageContent={pageContent} />
